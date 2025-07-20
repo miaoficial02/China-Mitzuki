b0b268958114114e99aed7e6a84e21d0e071e148
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-20 14:31:38 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -59,8 +59,8 @@ global.etiqueta = '👑 і𝗍s.mіᥲ.᥆𝖿іᥴіᥲᥣ 👑'
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.moneda = '¥enes'
-global.welcom1 = ' Edita Con El Comando setwelcome'
-global.welcom2 = ' Edita Con El Comando setbye'
+global.welcom1 = ' Edita Con El Comando #setwelcome'
+global.welcom2 = ' Edita Con El Comando #setbye'
 global.banner = 'https://files.catbox.moe/b7u1gt.png'
 global.avatar = 'https://files.catbox.moe/q06aba.jpg'
 
@@ -70,15 +70,15 @@ global.gp1 = 'https://chat.whatsapp.com/LrEuqWCkkjBIuNIHqC8UjB?mode=r_c'
 global.comunidad1 = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
 global.channel = 'https://whatsapp.com/channel/0029VbAfPu9BqbrEMFWXKE0d'
 global.channel2 = 'https://whatsapp.com/channel/0029VbAfPu9BqbrEMFWXKE0d'
-global.md = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot'
-global.correo = 'thekingdestroy507@gmail.com'
+global.md = 'https://github.com/míaoficial02/China-Mitzuki'
+global.correo = 'itss.mia.oficial@gmail.com'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
-ch1: '120363416409380841@newsletter',
+ch1: '12000000000000@newsletter',
 }
 global.multiplier = 60
 
